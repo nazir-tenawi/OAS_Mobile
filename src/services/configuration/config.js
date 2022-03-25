049bf8f3896969c124a0c7ceef5fb598df80df23
nazir-tenawi <52083577+nazir-tenawi@users.noreply.github.com>
2022-03-26 00:28:42 +0300

    1

---
+++ src/services/configuration/config.js
@@ -0,0 +1,20 @@
+
+//export const baseUrl= "http://192.168.43.113:54476"; 
+export const baseUrl= "https://ems.bancoder.com"; 
+
+
+
+export const urlDev = baseUrl+"/Api/AttendanceTracking/";
+export const urlogin =  baseUrl+"/Api/AttendanceTracking/";
+
+export const urlResource = baseUrl+"/UploadFiles/";
+
+export default {
+  url: urlDev,
+  loginurl:urlogin,
+  drawerMenuColor:"#000000",
+  appTitle:"EMS",
+  mapApiKey:"put your google map api key here",
+  pubnubSubscribeKey: "",
+  pubnubPublishKey: ""
+};
